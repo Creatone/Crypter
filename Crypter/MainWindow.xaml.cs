4cb80186f94a907cf0a387f6d93b846bf66efa72
Creatone <paul.szulik@gmail.com>
2017-05-15 20:23:21 +0200

    Initial commit with designed UI

---
+++ Crypter/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+namespace Crypter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void File_In_Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void File_Out_Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void File_Public_Keys_Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Encrypt_Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Decrypt_Button_Click(object sender, RoutedEventArgs e)
+        {
+         
+        }
+    }
+}
